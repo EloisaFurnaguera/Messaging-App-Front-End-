0e8ab4c19ca72d0d07f0b573b087653774870754
“Eloisa <49822691+EloisaFurnaguera@users.noreply.github.com>
2020-02-15 13:56:27 -0800

    Hearder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
-
+import Header from "./components/Header"
 import './App.css';
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Messasges</h1>
-      </div>
+      <Header />
+    
     )
   }
 }
